40e86b2af6610601955e45634f8ce5db6e51b37b
Carl Bednorz <carl@surfgreen.dev>
2021-01-08 19:35:09 +0100

    Content and HTML Setup for Sidebar

--- src/components/sidebar/Sidebar.vue
+++ src/components/sidebar/Sidebar.vue
@@ -1,12 +1,53 @@
 <template>
     <div class="bg-gray-700 h-full min-h-screen">
-        Here is the sidebar
+        <nav>
+            <ul>
+                <li v-for="navItem in navItems" :key="navItem.name">
+                    <a href="">{{ navItem.name }}</a>
+                </li>
+            </ul>
+        </nav>
+
+        <nav>
+            <ul>
+                <li v-for="supportNavItem in supportNavItems" :key="supportNavItem.name">
+                    <a href="">{{ supportNavItem.name }}</a>
+                </li>
+            </ul>
+        </nav>
+
+        <div>
+            <ul>
+                <li>
+                    <h5>Mediamar &copy; 2020</h5>
+                </li>
+                <li>
+                    <a href="">Privacy</a> &centerdot; <a href="">Terms & Condition</a>
+                </li>
+            </ul>
+        </div>
     </div>
 </template>
 
 <script>
 export default {
   name: 'Sidebar',
+  data() {
+    return {
+      navItems: [
+        { name: 'Home', isActive: 'false', icon: '' },
+        { name: 'Dispensary', isActive: 'false', icon: '' },
+        { name: 'Patients', isActive: 'true', icon: '' },
+        { name: 'Pharmacy Orders', isActive: 'false', icon: '' },
+        { name: 'Reports', isActive: 'false', icon: '' },
+        { name: 'Education Resources', isActive: 'false', icon: '' },
+      ],
+      supportNavItems: [
+        { name: 'FAQs', isActive: 'false', icon: '' },
+        { name: 'Help & Supprt', isActive: 'false', icon: '' },
+      ],
+    };
+  },
 };
 </script>
 
